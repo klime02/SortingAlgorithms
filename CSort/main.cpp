79e54d9be65ccde49b080260bb164ec53027b233
PommeFraiche <36454174+PommeFraiche@users.noreply.github.com>
2018-10-16 14:27:18 +0100

    Update main.cpp

--- CSort/main.cpp
+++ CSort/main.cpp
@@ -8,40 +8,41 @@ void insertionSort(int a[]);
 
 int main() {
 	//INPUT
-	vector<int> a(11);
-	a = (10, 54, 2, 3, 1, 7, 8, 9, 5, 4, 6);
-	//Print list
-	cout << "Here is your list; " << "\n";
-	cout << a << "\n";
-
-	//Memorize a before sorting it
-	int list[sizeof(a)] = a{};
-
-	//Insertion sort
-	insertionSort(a);
-	//Print sorted list
-	cout << "Let sort this list:" << "\n";
-	cout << a << "\n";
-
-	//Memorize sorted list
-	int sortedList[sizeof(a)] = a;
-
+	vector<int> array(11);
+	array = { 10, 54, 2, 3, 1, 7, 8, 9, 5, 4, 6 };
+	//Print array
+	cout << "Here is your array: ";
+	for (int i=0; i <= array.size()-1;i++) {
+		cout << array[i] << " ";
+	}
+	cout << "\n";
+	//New array
+	vector<int> sorted_array(array.size());
+	cout<< "Let sort this list:";
+	//Sort array and store it in sorted_array
+	sorted_array = insertionSort(array);                                                  
+	//Print sorted_array
+	for (int i = 0; i <= sorted_array.size() - 1; i++) {
+		cout << sorted_array[i] << " ";
+	}
+	cout << "\n";
+	return 0;
 
 }
 
 
-void insertionSort(int a[]) {
-	int n = sizeof(a);
+vector<int> insertionSort(vector<int> a) {
 	int x;
-	for (int i; i > n; i++) {
+	for (int i=0; i <= a.size()-1; i++) {
 		x = a[i];
 		int j = i;
 		while (j > 0 && a[j - 1] > x) {
 			a[j] = a[j - 1];
 			j--;
 		}
-		a[j] = x;
+		a[j]=x;
 	}
+	return a;
 }
 
 void initialPosition(int a[], int sorteda[]) {
