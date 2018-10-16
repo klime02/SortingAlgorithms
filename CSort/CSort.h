2488e457abc6819d677e989336a2673d64e5271a
klime <klime02@hotmail.com>
2018-10-16 14:41:04 +0100

    Added CSort.h and CSortTest.cpp

---
+++ CSort/CSort.h
@@ -0,0 +1,8 @@
+#ifndef CSort
+#define CSort
+#include<vector>
+using namespace std;
+
+vector<int> insertionSort(vector<int> a);
+
+#endif
---
+++ CSort/CSortTest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+#include"CSort.h"
+
+using namespace std;
+
+int main()
+{
+
+}
